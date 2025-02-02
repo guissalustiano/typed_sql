9e10e4bd76ebbf8b403c0b7e211d8f0b1558ac7c
Guilherme Salustiano <guissalustiano@gmail.com>
2025-02-02 20:23:50 -0300

    first test pass! Basic type solver

--- src/lib.rs
+++ src/lib.rs
@@ -1,134 +1,133 @@
-use core::panic;
+use pg_query::NodeEnum;
 
-use pg_query::{protobuf::ParseResult, NodeEnum};
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Type {
+    Int,
+    Text,
+}
+
+#[derive(Debug, Clone)]
+pub struct Column {
+    pub name: String,
+    pub type_: Type,
+}
 
 #[derive(Debug, Clone)]
 pub struct Table {
-    pub(crate) name: String,
-    pub(crate) columns: Vec<String>,
-}
-
-impl Table {
-    #[cfg(test)]
-    pub(crate) fn new<'a>(name: &'a str, columns: impl IntoIterator<Item = &'a str>) -> Self {
-        Table {
-            name: name.to_string(),
-            columns: columns.into_iter().map(ToString::to_string).collect(),
-        }
-    }
+    pub name: String,
+    pub columns: Vec<Column>,
 }
 
 #[derive(Debug)]
 pub struct Catalog {
-    pub(crate) tables: Vec<Table>,
+    pub tables: Vec<Table>,
 }
 
 impl Catalog {
     #[cfg(test)]
-    pub(crate) fn new() -> Self {
-        Catalog { tables: Vec::new() }
+    fn new(tables: Vec<Table>) -> Self {
+        Catalog { tables }
     }
 
-    pub(crate) fn table(&self, t_name: &str) -> Option<&Table> {
-        self.tables.iter().find(|t| t.name == t_name)
+    fn find_type(&self, t_name: &str, c_name: &str) -> Option<Type> {
+        self.tables
+            .iter()
+            .find(|t| t.name == t_name)?
+            .columns
+            .iter()
+            .find(|c| c.name == c_name)
+            .map(|c| c.type_)
     }
-
-    pub(crate) fn insert(mut self, t: Table) -> Self {
-        self.tables.push(t);
-        self
-    }
-}
-
-fn full_qualify(sc: &Catalog, ast: ParseResult) -> Result<ParseResult, ()> {
-    for stmt in &ast.stmts {
-        let stmt = stmt
-            .stmt
-            .as_ref()
-            .expect(".stmt")
-            .node
-            .as_ref()
-            .expect(".stmt.node");
-
-        match stmt {
-            NodeEnum::SelectStmt(s) => {
-                let from = &s
-                    .from_clause
-                    .iter()
-                    .map(|n| {
-                        let NodeEnum::RangeVar(r) = n.node.as_ref().expect("stmt.node.from.node")
-                        else {
-                            unimplemented!("from")
-                        };
-                        dbg!(r)
-                    })
-                    .map(|r| sc.table(&r.relname).expect("invalid table"))
-                    .collect::<Vec<_>>();
-
-                for target in &s.target_list {
-                    let NodeEnum::ResTarget(target) =
-                        target.node.as_ref().expect("stmt.node.target")
-                    else {
-                        unimplemented!("target")
-                    };
-                    let target = target
-                        .val
-                        .as_ref()
-                        .expect("target.val")
-                        .node
-                        .as_ref()
-                        .expect("target.val.node");
-
-                    match target {
-                        NodeEnum::ColumnRef(cr) => {
-                            let target = &mut cr.fields.iter().map(|f| {
-                                match f.node.as_ref().expect("column_ref.node") {
-                                    NodeEnum::String(pg_query::protobuf::String { sval }) => {
-                                        dbg!(sval)
-                                    }
-                                    _ => unimplemented!("column ref"),
-                                }
-                            });
-
-                            // let target = cr.fields.
-
-                            // match from.as_slice() {
-                            //     &[] => unreachable!("fields len is zero"),
-                            //     // column
-                            //     &[_] => {}
-                            //     // table.column
-                            //     &[t_name, c_name] => {
-                            //         // contains
-                            //         let Some(t) = from.iter().find(|t| t.name == t_name) else {
-                            //             panic!("table not found");
-                            //         };
-
-                            //         assert!(t.columns.contains(c_name))
-                            //         // let table
-                            //     }
-                            //     // schema.table.column
-                            //     &[_, _, _] => unimplemented!("fields with schema"),
-                            //     // that's a lie, we can resolve to structs
-                            //     _ => panic!("to many does not resolve to anything"),
-                            // }
-
-                            dbg!(cr);
-                            todo!("todo")
-                        }
-                        _ => unimplemented!("column"),
-                    }
-                }
-                todo!()
-            }
-            _ => unimplemented!("stmt"),
-        }
-    }
-
-    Err(())
 }
 
 #[cfg(test)]
-fn parse(sql: &str) -> ParseResult {
-    pg_query::parse(sql).unwrap().protobuf
+fn parse(sql: &str) -> NodeEnum {
+    pg_query::parse(sql)
+        .unwrap()
+        .protobuf
+        .stmts
+        .first()
+        .unwrap()
+        .stmt
+        .as_ref()
+        .unwrap()
+        .node
+        .as_ref()
+        .unwrap()
+        .clone()
+}
+
+#[cfg(test)]
+macro_rules! tables {
+    (
+        $($table_name:ident {
+            $($column_name:ident => $column_type:ident),* $(,)?
+        }),* $(,)?
+    ) => {
+        {
+            let mut tables = Vec::new();
+
+            $(
+                let mut columns = Vec::new();
+
+                $(
+                    columns.push(Column {
+                        name: stringify!($column_name).to_string(),
+                        type_: Type::$column_type,
+                    });
+                )*
+
+                tables.push(Table {
+                    name: stringify!($table_name).to_string(),
+                    columns: columns,
+                });
+            )*
+
+            tables
+        }
+    };
+}
+
+pub(crate) fn solve_type(ctg: &Catalog, stmt: NodeEnum) -> Vec<Type> {
+    match stmt {
+        NodeEnum::SelectStmt(s) => s
+            .target_list
+            .iter()
+            .map(|target| {
+                let NodeEnum::ResTarget(target) = target.node.as_ref().expect("stmt.node.target")
+                else {
+                    unimplemented!("target")
+                };
+                let target = target
+                    .val
+                    .as_ref()
+                    .expect("target.val")
+                    .node
+                    .as_ref()
+                    .expect("target.val.node");
+
+                match target {
+                    NodeEnum::ColumnRef(cr) => {
+                        let &[t_name, c_name] = &cr
+                            .fields
+                            .iter()
+                            .map(|f| match f.node.as_ref().expect("column_ref.node") {
+                                NodeEnum::String(pg_query::protobuf::String { sval }) => sval,
+                                _ => unimplemented!("column ref"),
+                            })
+                            .collect::<Vec<_>>()[..]
+                        else {
+                            panic!("invalid name, use table.column")
+                        };
+
+                        ctg.find_type(t_name, c_name).expect("column not found")
+                    }
+                    _ => unimplemented!("column"),
+                }
+            })
+            .collect(),
+        _ => unimplemented!("stmt"),
+    }
 }
 
 #[cfg(test)]
@@ -138,11 +137,16 @@ mod tests {
 
     #[test]
     fn resolve_simple_select() {
-        let ctl = Catalog::new().insert(Table::new("x", ["a", "b", "c"]));
+        let ctl = Catalog::new(tables!(
+            x {
+                a => Text,
+                b => Int,
+            },
+        ));
 
-        let ast = parse("SELECT x.a, b FROM x");
-        let f_ast = parse("SELECT x.a, x.b FROM x");
+        let ast = parse("SELECT x.a, x.b FROM x");
+        let expected = vec![Type::Text, Type::Int];
 
-        assert_eq!(full_qualify(&ctl, ast), Ok(f_ast))
+        assert_eq!(solve_type(&ctl, ast), expected);
     }
 }
